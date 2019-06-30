71656d42d68bdb7fcd4b964c9515cfa6cefcf992
Stanislav Kaisin <8050361@gmail.com>
2019-06-30 15:28:00 +0300

    fixed bugs

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageFinder from '../image-finder/ImageFinder/ImageFinder';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <ImageFinder />
+    </div>
+  );
+};
+
+export default App;
